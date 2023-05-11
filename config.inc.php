2c9283ed5e1ba3995e6a50ae0c435c3c77e74a67
frankchieng <kurtqian@gmail.com>
2023-05-11 20:33:32 +0800

    Add files via upload
    the global configuration file

---
+++ config.inc.php
@@ -0,0 +1,10 @@
+<?php
+//stability api秘钥
+define('STABILITYAI_APP_KEY','');//use your own api key
+define('STABILITYAI_BASE_URL','https://api.stability.ai');
+define('STABILITYAI_ENGINE_LIST_URL', STABILITYAI_BASE_URL.'/v1/engines/list');
+define('DEFAULT_STABILITY_ENGINE','stable-diffusion-512-v2-1');
+$SD_SAMPLER = array('DDIM', 'DDPM', 'K_DPMPP_2M','K_DPMPP_2S_ANCESTRAL', 'K_DPM_2', 'K_DPM_2_ANCESTRAL', 'K_EULER', 'K_EULER_ANCESTRAL', 'K_HEUN','K_LMS');
+define('INIT_IMAGE_MODE','IMAGE_STRENGTH');
+$SD_ENGINE_LIST = array('stable-diffusion-v1-5', 'stable-diffusion-512-v2-0', 'stable-diffusion-768-v2-0', 'stable-diffusion-depth-v2-0', 'stable-diffusion-512-v2-1', 'stable-diffusion-768-v2-1', 'stable-diffusion-xl-beta-v2-2-2');
+?>
